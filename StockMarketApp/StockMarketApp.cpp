ce0da436a1f7a825f395289ae1a877e35768fc17
haris-afr <haris.afridi1312@gmail.com>
2024-12-08 11:42:00 +0500

    Add project files.

---
+++ StockMarketApp/StockMarketApp.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+int main()
+{
+    //creates a window and titles it 
+    sf::RenderWindow window(sf::VideoMode(600, 480), "Stock Market App", sf::Style::Close || sf::Style::Titlebar);
+    return 0;
+
+    while (window.isOpen()) {
+
+    }
+}
