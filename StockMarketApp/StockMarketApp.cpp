092c67c20c5af1000232a8d63a7edf41c45a36d5
haris-afr <haris.afridi1312@gmail.com>
2024-12-08 17:34:42 +0500

    add events

--- StockMarketApp/StockMarketApp.cpp
+++ StockMarketApp/StockMarketApp.cpp
@@ -4,10 +4,34 @@ using namespace std;
 int main()
 {
     //creates a window and titles it 
-    sf::RenderWindow window(sf::VideoMode(600, 480), "Stock Market App", sf::Style::Close || sf::Style::Titlebar);
-    return 0;
+    sf::Window window(sf::VideoMode(1200, 600), "Stock Market App", sf::Style::Close | sf::Style::Titlebar);
+    sf::RectangleShape button(sf::Vector2f(100, 50));
 
     while (window.isOpen()) {
 
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // check the type of the event...
+            switch (event.type)
+            {
+                // window closed
+            case sf::Event::Closed:
+                window.close();
+                break;
+
+
+            default:
+                break;
+            }
+        }
+
+        window.draw(button);
+        window.display();
+
     }
+
+    return 0;
+
+   
 }
