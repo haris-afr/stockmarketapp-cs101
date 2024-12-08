c3f01b924990b904212d1d5afddc070dc022b33d
haris-afr <haris.afridi1312@gmail.com>
2024-12-08 22:48:01 +0500

    add blank graph

--- StockMarketApp/StockMarketApp.cpp
+++ StockMarketApp/StockMarketApp.cpp
@@ -1,15 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     //creates a window and titles it 
-    sf::Window window(sf::VideoMode(1200, 600), "Stock Market App", sf::Style::Close | sf::Style::Titlebar);
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "Stock Market App", sf::Style::Close | sf::Style::Titlebar);
     sf::RectangleShape button(sf::Vector2f(100, 50));
+
+    sf::Texture chartTexture;
+    sf::Sprite chartSprite;
+ 
+    if (!chartTexture.loadFromFile("assets/chart-blank.png")) {
+        cout << "can;t print" << endl;
+    }
+    chartSprite.setTexture(chartTexture);
+
 
     while (window.isOpen()) {
 
         sf::Event event;
+        //this is used to create the event loop
         while (window.pollEvent(event))
         {
             // check the type of the event...
@@ -26,8 +37,13 @@ int main()
             }
         }
 
+        button.setFillColor(sf::Color(154, 166, 178));
+        window.clear(sf::Color(217, 234, 253, 0));
+
         window.draw(button);
+        window.draw(chartSprite);
         window.display();
+
 
     }
 
