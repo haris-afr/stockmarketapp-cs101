12652799bffcea2062bfc458d3c600d9f117b381
haris-afr <haris.afridi1312@gmail.com>
2024-12-09 11:18:23 +0500

    base for stock options

--- StockMarketApp/StockMarketApp.cpp
+++ StockMarketApp/StockMarketApp.cpp
@@ -6,7 +6,24 @@ int main()
 {
     //creates a window and titles it 
     sf::RenderWindow window(sf::VideoMode(1600, 900), "Stock Market App", sf::Style::Close | sf::Style::Titlebar);
-    sf::RectangleShape button(sf::Vector2f(100, 50));
+
+    sf::Texture T_StockOption_Coin;
+    sf::Texture T_StockOption_Tech1;
+    sf::Texture T_StockOption_Tech2;
+    sf::Texture T_StockOption_Insurance;
+    sf::Texture T_StockOption_War;
+    sf::Texture T_StockOption_Gold;
+    sf::Texture T_StockOption_Dollar;
+    sf::Texture T_StockOption_Oil;
+
+    sf::Sprite S_StockOption_Coin;
+    sf::Sprite S_StockOption_Tech1;
+    sf::Sprite S_StockOption_Tech2;
+    sf::Sprite S_StockOption_Insurance;
+    sf::Sprite S_StockOption_War;
+    sf::Sprite S_StockOption_Gold;
+    sf::Sprite S_StockOption_Dollar;
+    sf::Sprite S_StockOption_Oil;
 
     sf::Texture chartTexture;
     sf::Sprite chartSprite;
@@ -36,12 +53,14 @@ int main()
                 break;
             }
         }
-
-        button.setFillColor(sf::Color(154, 166, 178));
         window.clear(sf::Color(217, 234, 253, 0));
 
-        window.draw(button);
         window.draw(chartSprite);
+        
+        
+        
+        
+        
         window.display();
 
 
