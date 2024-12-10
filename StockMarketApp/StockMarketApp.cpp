3afbf8b5d658844f278b1c797fa488c2c9fbf381
haris-afr <haris.afridi1312@gmail.com>
2024-12-10 22:50:09 +0500

    draw eye, add eye class

--- StockMarketApp/StockMarketApp.cpp
+++ StockMarketApp/StockMarketApp.cpp
@@ -1,12 +1,52 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
 using namespace std;
+
+class visEye {
+public:
+    sf::Texture T_EyeClosed;
+    sf::Texture T_EyeOpen;
+    bool CurrentTexture;
+    sf::Sprite S_CurrentSprite;
+
+    void initialize() {
+        T_EyeClosed.loadFromFile("assets/eye-close.png");
+        T_EyeOpen.loadFromFile("assets/eye-open.png");
+        S_CurrentSprite.setTexture(T_EyeClosed);
+        CurrentTexture = 0;
+    }
+
+    void setState(bool state) {
+        if (state == 0) {
+            S_CurrentSprite.setTexture(T_EyeClosed);
+            CurrentTexture = 0;
+        }
+        else {
+            S_CurrentSprite.setTexture(T_EyeOpen);
+            CurrentTexture = 1;
+        }
+    }
+
+    void flip() {
+        if (CurrentTexture == 0) {
+            S_CurrentSprite.setTexture(T_EyeOpen);
+            CurrentTexture = 1;
+        }
+        else {
+            S_CurrentSprite.setTexture(T_EyeClosed);
+            CurrentTexture = 0;
+        }
+    }
+
+};
+
 
 int main()
 {
     //creates a window and titles it 
     sf::RenderWindow window(sf::VideoMode(1600, 900), "Stock Market App", sf::Style::Close | sf::Style::Titlebar);
-
+    
     sf::Texture T_StockOption_Coin;
     sf::Texture T_StockOption_Tech1;
     sf::Texture T_StockOption_Tech2;
@@ -15,8 +55,7 @@ int main()
     sf::Texture T_StockOption_Gold;
     sf::Texture T_StockOption_Dollar;
     sf::Texture T_StockOption_Oil;
-    sf::Texture T_EyeClosed;
-    sf::Texture T_EyeOpen;
+
 
     sf::Sprite S_StockOption_Coin;
     sf::Sprite S_StockOption_Tech1;
@@ -26,11 +65,14 @@ int main()
     sf::Sprite S_StockOption_Gold;
     sf::Sprite S_StockOption_Dollar;
     sf::Sprite S_StockOption_Oil;
+    
 
     sf::Texture chartTexture;
     sf::Sprite chartSprite;
- 
     chartTexture.loadFromFile("assets/chart-blank.png");
+    chartSprite.setTexture(chartTexture);
+    
+    
     T_StockOption_Coin.loadFromFile("assets/coin.png");
     T_StockOption_Tech1.loadFromFile("assets/apple.png");
     T_StockOption_Tech2.loadFromFile("assets/google.png");
@@ -39,11 +81,7 @@ int main()
     T_StockOption_Gold.loadFromFile("assets/gold.png");
     T_StockOption_Dollar.loadFromFile("assets/dollar.png");
     T_StockOption_Oil.loadFromFile("assets/oil.png");
-    T_EyeClosed.loadFromFile("assets/eye-close.png");
-    T_EyeOpen.loadFromFile("assets/eye-open.png");
 
-
-    chartSprite.setTexture(chartTexture);
     S_StockOption_Coin.setTexture(T_StockOption_Coin);
     S_StockOption_Tech1.setTexture(T_StockOption_Tech1);
     S_StockOption_Tech2.setTexture(T_StockOption_Tech2);
@@ -61,9 +99,13 @@ int main()
     S_StockOption_Gold.setPosition(1225, 300);
     S_StockOption_Dollar.setPosition(850, 400);
     S_StockOption_Oil.setPosition(1225, 400);
+    
+    
+    visEye visEyeArray[8];
+    for (int i = 0; i < 8; i++) {
+        visEyeArray[i].initialize();
+    }
 
-    
-    
     bool visibilityArray[8] = {0};
 
 
@@ -89,6 +131,7 @@ int main()
         window.clear(sf::Color(217, 234, 253, 0));
 
         window.draw(chartSprite);
+        
         window.draw(S_StockOption_Coin);
         window.draw(S_StockOption_Tech1);
         window.draw(S_StockOption_Tech2);
@@ -98,9 +141,9 @@ int main()
         window.draw(S_StockOption_Dollar);
         window.draw(S_StockOption_Oil);
         
-        
-        
-        
+        for (int i = 0; i < 8; i++) {
+            window.draw(visEyeArray[i].S_CurrentSprite);
+        }
         
         window.display();
 
